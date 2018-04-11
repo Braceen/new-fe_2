e247354205adcaaee3f2915ba714bf2281d21d30
Alex <jjbumers@gmail.com>
2018-04-11 13:12:21 +0300

    Maybe last change for header style and js

--- build/main.js
+++ build/main.js
@@ -3,5 +3,14 @@ window.onload = function () {
    var visible = document.querySelector('.ul_min');
    gamburger.onclick = function () {
        visible.classList.toggle('visible');
+   };
+
+   var gps_hover = document.querySelector('.gps_hover');
+   var gps = document.querySelector('.gps');
+   gps_hover.onmouseover = function () {
+       gps.classList.toggle('visiblee');
+   }
+   gps_hover.onmouseout = function () {
+       gps.classList.remove('visiblee');
    }
 };
